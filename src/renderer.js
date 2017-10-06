339c72933257b802778866d23d7f1431b1497634
Alex Karadimos <karadalex@gmail.com>
2017-10-07 00:23:56 +0300

    Receive text input, send it to Hospbot server, get response and display it.

--- src/renderer.js
+++ src/renderer.js
@@ -3,36 +3,75 @@
 // All of the Node.js APIs are available in this process.
 
 var botui = new BotUI('hosbpot-ui');
+var request = require("request");
 
 function init() {
     botui.message
-      .bot({
-        delay: 700,
-        content: 'What would you like to do?'
-      })
-      .then(function () {
-        return botui.action.button({
-          delay: 1000,
-          action: [{
-            text: 'Talk to Hospbot',
-            value: 'talk'
-          }, {
-            text: 'Check eHOSP',
-            value: 'check'
-          }]
+        .bot({
+            delay: 700,
+            content: 'What would you like to do?'
         })
-    }).then(function (res) {
-      if(res.value == 'check') {
-        botui.message.bot({
-            delay: 1200,
-            content: "Check eHOSP"
-        }).then(init);
-      } else {
-        botui.message.bot({
-          delay: 1200,
-          content: "Hello from Hospbot"
-        }).then(init);
-      }
+        .then(function () {
+            return botui.action.button({
+                delay: 1000,
+                action: [
+                    {
+                        text: 'Talk to Hospbot',
+                        value: 'talk'
+                    }, 
+                    {
+                        text: 'Check eHOSP',
+                        value: 'check'
+                    }
+                ]
+            })
+        })
+        .then(function (res) {
+            if(res.value == 'check') {
+                botui.message.bot({
+                    delay: 1200,
+                    content: "Check eHOSP"
+                }).then(init);
+            } else {
+                botui.action.text({ // show 'text' action
+                    action: {
+                        placeholder: 'Your message ...'
+                    }
+                })
+                .then(function (res) {
+                    msgFromHospbot = botui.message.bot({
+                        delay: 200,
+                        loading: true
+                    }).then(function (index) {
+                        msgFromHospbot = index;
+                        sendMessage(res.value);
+                    });
+                })
+            }
+        });
+}
+
+function sendMessage(msg) {
+    var options = {
+        method: 'POST',
+        url: 'http://hospbot.azurewebsites.net/api/v1/ehosp_webhookA32D7FCAAF7B912B8224EEC441DC9',
+        headers: {
+            'cache-control': 'no-cache',
+            'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        },
+        formData: {
+            message: msg
+        }
+    };
+    
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+        // Display message from Hospbot
+        botui.message.update(msgFromHospbot, {
+            content: body
+        })
+        .then(init);
     });
 }
 
